89183e63d56a08fcdc9a47ffcbd6d1f210b4fd20
Piotr Gołębiewski <cryptomute@gmail.com>
2016-02-16 14:38:36 +0100

    Better API and wider test coverage

--- tests/CryptomuteTest.php
+++ tests/CryptomuteTest.php
@@ -8,128 +8,174 @@ use PHPUnit_Framework_TestCase;
 
 class CryptomuteTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Cryptomute
-     */
-    protected $cryptomute;
+    const MIN_VALUE = '0';
+    const MAX_VALUE = '9999999999';
+    const CIPHER = 'aes-128-cbc';
 
-    /**
-     * @var string
-     */
-    protected $maxValue;
-
-    public function setUp()
-    {
-        $this->minValue = '0';
-        $this->maxValue = '9999999999';
-        $cipher = 'aes-128-cbc';
-        $password = openssl_random_pseudo_bytes(32);
-        $key = openssl_random_pseudo_bytes(32);
-        $rounds = 7;
-        $iv = openssl_random_pseudo_bytes(16);
-
-        $this->cryptomute = new Cryptomute($this->minValue, $this->maxValue, $cipher, $password, $key, $rounds, $iv);
-    }
+    public static $testedRounds = [
+        3  => 'minimum',
+        5  => 'normal',
+        7  => 'recommended',
+        11 => 'insane',
+    ];
 
     public function testReadmeExample()
     {
-        $cryptomute = new Cryptomute(
-            '0',                // minimum value
-            '999999999',        // maximum value
-            'aes-128-cbc',      // cipher
-            '0123456789qwerty', // password
-            '0123456789zxcvbn', // key
-            7,                  // number of rounds
-            '0123456789abcdef'  // initialization vector
-        );
+        $message = '[%d / %s] Expected README example to work.';
 
-        $plainValue = '2048';
-        $encoded = $cryptomute->encrypt($plainValue, 'dec');
-        $decoded = $cryptomute->decrypt($encoded, 'dec');
+        foreach (self::$testedRounds as $rounds => $description) {
+            $cryptomute = $this->getCryptomute($rounds);
+            $password = openssl_random_pseudo_bytes(32);
+            $iv = openssl_random_pseudo_bytes(16);
 
-        $this->assertEquals($plainValue, $decoded);
+            $plainValue = '2048';
+            $encoded = $cryptomute->encrypt($plainValue, 10, false, $password, $iv);
+            $decoded = $cryptomute->decrypt($encoded, 10, false, $password, $iv);
+
+            $this->assertEquals($plainValue, $decoded, sprintf(
+                $message,
+                $rounds,
+                $description
+            ));
+        }
     }
 
     public function testEncodedValuesAreInDomain()
     {
+        $message = '[%d / %s] Base %d value of %s encoded to %s is still in domain %s - %s.';
         $minVal = 100;
         $maxVal = 999;
 
-        $cryptomute = new Cryptomute(
-            (string) $minVal,   // minimum value
-            (string) $maxVal,   // maximum value
-            'aes-128-cbc',      // cipher
-            '0123456789qwerty', // password
-            '0123456789zxcvbn', // key
-            7,                  // number of rounds
-            '0123456789abcdef'  // initialization vector
+        foreach (self::$testedRounds as $rounds => $description) {
+            $cryptomute = $this->getCryptomute($rounds, "$minVal", "$maxVal");
+            $password = openssl_random_pseudo_bytes(32);
+
+            foreach (Cryptomute::$allowedBases as $base => $pattern) {
+                for ($i = $minVal; $i <= $maxVal; $i++) {
+                    $iv = openssl_random_pseudo_bytes(16);
+
+                    $input = gmp_strval(gmp_init("$i", 10), $base);
+                    $encoded = $cryptomute->encrypt($input, $base, false, $password, $iv);
+                    $intVal = (int)gmp_strval(gmp_init($encoded, $base), 10);
+
+                    $this->assertTrue(
+                        $minVal <= $intVal && $intVal <= $maxVal,
+                        sprintf($message, $rounds, $description, $base, $i, $encoded, $minVal, $maxVal)
+                    );
+                }
+            }
+        }
+    }
+
+    public function testDecodesEncodedNumbers()
+    {
+        $message = '[%d / %s] Encoded base %d value must decode to initial value [%s].';
+
+        foreach (self::$testedRounds as $rounds => $description) {
+            $cryptomute = $this->getCryptomute($rounds);
+            $password = openssl_random_pseudo_bytes(32);
+
+            foreach (Cryptomute::$allowedBases as $base => $pattern) {
+                for ($i = 0; $i < 10; $i++) {
+                    $iv = openssl_random_pseudo_bytes(16);
+
+                    $input = gmp_strval(gmp_random_range(
+                        gmp_init(self::MIN_VALUE, 10),
+                        gmp_init(self::MAX_VALUE, 10)
+                    ), $base);
+
+                    $encrypted = $cryptomute->encrypt($input, $base, false, $password, $iv);
+                    $decrypted = $cryptomute->decrypt($encrypted, $base, false, $password, $iv);
+
+                    $this->assertEquals($input, $decrypted, sprintf(
+                        $message,
+                        $rounds,
+                        $description,
+                        $base,
+                        $input
+                    ));
+                }
+            }
+        }
+    }
+
+    public function testEncodeWithDiffrentPasswordsProducesDiffrentResults()
+    {
+        $message = '[%d / %s] Same base %d value encoded with diffrent passwords produce diffrent results [%s].';
+
+        foreach (self::$testedRounds as $rounds => $description) {
+            $cryptomute = $this->getCryptomute($rounds);
+
+            foreach (Cryptomute::$allowedBases as $base => $pattern) {
+                for ($i = 0; $i < 10; $i++) {
+                    $iv = openssl_random_pseudo_bytes(16);
+
+                    $input = gmp_strval(gmp_random_range(
+                        gmp_init(self::MIN_VALUE, 10),
+                        gmp_init(self::MAX_VALUE, 10)
+                    ), $base);
+
+                    $encrypted1 = $cryptomute->encrypt($input, $base, false, 'foo', $iv);
+                    $encrypted2 = $cryptomute->encrypt($input, $base, false, 'bar', $iv);
+
+                    $this->assertNotEquals($encrypted1, $encrypted2, sprintf(
+                        $message,
+                        $rounds,
+                        $description,
+                        $base,
+                        $input
+                    ));
+                }
+            }
+        }
+    }
+
+    public function testDecodeWithDiffrentPasswordsProducesDiffrentResults()
+    {
+        $message = '[%d / %s] Same base %d value decoded with diffrent passwords produce diffrent results [%s].';
+
+        foreach (self::$testedRounds as $rounds => $description) {
+            $cryptomute = $this->getCryptomute($rounds);
+
+            foreach (Cryptomute::$allowedBases as $base => $pattern) {
+                for ($i = 0; $i < 10; $i++) {
+                    $iv = openssl_random_pseudo_bytes(16);
+
+                    $input = gmp_strval(gmp_random_range(
+                        gmp_init(self::MIN_VALUE, 10),
+                        gmp_init(self::MAX_VALUE, 10)
+                    ), $base);
+
+                    $decrypted1 = $cryptomute->decrypt($input, $base, false, 'foo', $iv);
+                    $decrypted2 = $cryptomute->decrypt($input, $base, false, 'bar', $iv);
+
+                    $this->assertNotEquals($decrypted1, $decrypted2, sprintf(
+                        $message,
+                        $rounds,
+                        $description,
+                        $base,
+                        $input
+                    ));
+                }
+            }
+        }
+    }
+
+    /**
+     * @param int         $rounds
+     * @param string|null $minVal
+     * @param string|null $maxVal
+     *
+     * @return Cryptomute
+     */
+    private function getCryptomute($rounds, $minVal = null, $maxVal = null)
+    {
+        return new Cryptomute(
+            $minVal ?: self::MIN_VALUE,
+            $maxVal ?: self::MAX_VALUE,
+            self::CIPHER,
+            openssl_random_pseudo_bytes(32),
+            $rounds
         );
-
-        $message = 'Value %s encoded to %s is still in domain %s - %s.';
-
-        for ($i = $minVal; $i <= $maxVal; $i++) {
-            $plainValue = "$i";
-            $encoded = $cryptomute->encrypt($plainValue, 'dec');
-            $intVal = intval($encoded);
-
-            $this->assertTrue(
-                $minVal <= $intVal && $intVal <= $maxVal,
-                sprintf($message, $plainValue, $encoded, $minVal, $maxVal)
-            );
-        }
-    }
-
-    public function testDecodesEncodedBinaryNumbers()
-    {
-        for ($i = 0; $i < 10; $i++) {
-            $input = gmp_strval(gmp_random_range(
-                gmp_init($this->minValue, 10),
-                gmp_init($this->maxValue, 10)
-            ), 2);
-
-            $encrypted = $this->cryptomute->encrypt($input, 'bin');
-            $decrypted = $this->cryptomute->decrypt($encrypted, 'bin');
-
-            $this->assertEquals($input, $decrypted, sprintf(
-                'Encoded binary value must decode to initial value [%s].',
-                $input
-            ));
-        }
-    }
-
-    public function testDecodesEncodedDecimalNumbers()
-    {
-        for ($i = 0; $i < 10; $i++) {
-            $input = gmp_strval(gmp_random_range(
-                gmp_init($this->minValue, 10),
-                gmp_init($this->maxValue, 10)
-            ), 10);
-
-            $encrypted = $this->cryptomute->encrypt($input, 'dec');
-            $decrypted = $this->cryptomute->decrypt($encrypted, 'dec');
-
-            $this->assertEquals($input, $decrypted, sprintf(
-                'Encoded decimal value must decode to initial value [%s].',
-                $input
-            ));
-        }
-    }
-
-    public function testDecodesEncodedHexadecimalNumbers()
-    {
-        for ($i = 0; $i < 10; $i++) {
-            $input = gmp_strval(gmp_random_range(
-                gmp_init($this->minValue, 10),
-                gmp_init($this->maxValue, 10)
-            ), 16);
-
-            $encrypted = $this->cryptomute->encrypt($input, 'hex');
-            $decrypted = $this->cryptomute->decrypt($encrypted, 'hex');
-
-            $this->assertEquals($input, $decrypted, sprintf(
-                'Encoded decimal value must decode to initial value [%s].',
-                $input
-            ));
-        }
     }
 }
